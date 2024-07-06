ab2ca722285a06c607b83d597245f9dada0b0cda
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-07-06 21:04:12 +0900

    ルートでのテストを追加

--- RailsTube/spec/request/static_pages_controller_spec.rb
+++ RailsTube/spec/request/static_pages_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  describe "GET /" do
+    it "responds successfully with an HTTP 200 status code" do
+      get root_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
   describe "GET /home" do
     it "responds successfully with an HTTP 200 status code" do
       get home_path
