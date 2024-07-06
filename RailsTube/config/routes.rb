7cf505ae33df77bd9b297039dba59a70fcb6c3f2
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-07-06 22:03:07 +0900

    今は必要ないのでコメントアウト

--- RailsTube/config/routes.rb
+++ RailsTube/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/home', to: 'static_pages#home', as: :home
-  get '/short', to: 'static_pages#short'
-  get '/channels', to: 'static_pages#channels' 
+  # get '/short', to: 'static_pages#short'
+  # get '/channels', to: 'static_pages#channels' 
+  
 end
--- RailsTube/spec/requests/static_pages_spec.rb
+++ RailsTube/spec/requests/static_pages_spec.rb
@@ -15,17 +15,17 @@ RSpec.describe "StaticPages", type: :request do
     end
   end
 
-  describe "GET /short" do
-    it "responds successfully with an HTTP 200 status code" do
-      get short_path
-      expect(response).to have_http_status(200)
-    end
-  end
+  # describe "GET /short" do
+  #   it "responds successfully with an HTTP 200 status code" do
+  #     get short_path
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
   
-  describe "GET /channels" do
-    it "responds successfully with an HTTP 200 status code" do
-      get channels_path
-      expect(response).to have_http_status(200)
-    end
-  end
+  # describe "GET /channels" do
+  #   it "responds successfully with an HTTP 200 status code" do
+  #     get channels_path
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
 end
