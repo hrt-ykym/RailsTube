41a90f2a047f48aeed3c57427d463b99e25b33d9
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-07-14 21:44:23 +0900

    destroy実装

--- RailsTube/app/controllers/sessions_controller.rb
+++ RailsTube/app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       reset_session
       log_in(user)
-      redirect_to user
+      redirect_to user_path(user)
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
@@ -16,5 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
